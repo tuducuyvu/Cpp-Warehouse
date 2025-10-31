849bcd154ddd0b48325723d279b49c8535565d38
escobrand <109648964+tuducuyvu@users.noreply.github.com>
2025-10-31 20:52:38 +0700

    lil change

--- Gấu Trúc Dữ Liệu/Convex Hull Trick/Convex Hull Trick - Constrained Container.cpp
+++ Gấu Trúc Dữ Liệu/Convex Hull Trick/Convex Hull Trick - Constrained Container.cpp
@@ -1,6 +1,5 @@
 // Convex Hull Trick - Constrained Container
 /* documents
-  tài liệu của lanciu
   https://usaco.guide/plat/convex-hull-trick
   https://cp-algorithms.com/geometry/convex_hull_trick.html
   https://wiki.vnoi.info/algo/dp/cht
@@ -18,20 +17,17 @@ using namespace std;
 typedef pair<ll,ll> pii;
 
 const int maxn = 3e5 + 10;
-pii a[maxn];
-vector<pii> b;
-ll dp[maxn];
 
 //Code Convex Hull Trick - Constrained Container
 struct Line 
 {
     ll a,b;
-    Line():a(0),b(0){} // Default constructor
-    Line(ll A,ll B):a(A),b(B){} // Parameterized constructor
+    Line():a(0),b(0){} 
+    Line(ll A,ll B):a(A),b(B){} 
     
       // Calculate y = ax + b for given x
     ll cal(ll x)const {return a * x + b;}
-      // Compute x coordinate of intersection between two lines
+      // Compute x coordinate of intersection of two lines
     friend ld xcut(Line a,Line b) { return (ld)(b.b - a.b) / abs(a.a - b.a);}
       // Check if the Line b in the hull is valid
     friend bool check(Line a,Line b,Line c) { return xcut(a,b) < xcut(b,c);}
@@ -52,12 +48,17 @@ struct CHT_Constrained :deque<Line>
     ll cal(ll x)
     {
         if(empty())return LLONG_MAX; // If no lines, return infinity
-          // Pop lines from front while next line gives smaller value
+          // Pop useless lines
         while(size()>1 && xcut(at(0),at(1)) < x)pop_front();
-        return at(0).cal(x); // Return value from optimal line
+        return at(0).cal(x); 
     }
 };
 //End Code Convex Hull Trick - Constrained Container
+
+
+pii a[maxn];
+vector<pii> b;
+ll dp[maxn];
 
 int main()
 {
@@ -71,35 +72,33 @@ int main()
     
     for(int i = 0;i<n;i++)
     {
-      cin>>a[i].fi>>a[i].se;
+        cin>>a[i].fi>>a[i].se;
     }
     
     sort(a,a+n,greater<>());
     
     for(int i = 0;i<n;i++)
     {
-      if(a[i].se>mx)
-      {
-        mx=a[i].se;
-        b.pb(a[i]);
-      }
+        if(a[i].se>mx)
+        {
+            mx=a[i].se;
+            b.pb(a[i]);
+        }
     }
     
     n = b.size();
     int i = 1;
     
     CHT_Constrained container; 
-    
-    
+        
     for(pii k : b)
     {
-      
-      dp[i] = dp[i-1] + k.fi * k.se;
-        
-      if(container.size()) dp[i] = min( dp[i], container.cal(k.se) ); // Use CHT to get minimum possible value if available
-        
-      container.add( Line(k.fi,dp[i-1]) ); // Add new line to CHT for future queries
-      i++;
+        dp[i] = dp[i-1] + k.fi * k.se;
+          
+        if(container.size()) dp[i] = min( dp[i], container.cal(k.se) ); // Use CHT to get minimum possible value if available
+          
+        container.add( Line(k.fi,dp[i-1]) ); // Add new line to CHT for future queries
+        i++;
     }
     
     cout<<dp[n];
