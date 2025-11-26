34c7d74aba02a58078ecc2d3471e08157431b9cf
escobrand <109648964+tuducuyvu@users.noreply.github.com>
2025-11-26 21:49:25 +0700

    change Convex Hull Trick implementation
    Updated Convex Hull Trick implementation with improved comments and structure.

--- Gấu Trúc Dữ Liệu/Convex Hull Trick/Convex Hull Trick - General Container.cpp
+++ Gấu Trúc Dữ Liệu/Convex Hull Trick/Convex Hull Trick - General Container.cpp
@@ -23,10 +23,9 @@ const int maxn = 1e5 + 10;
 struct Line // y = ax + b
 {
     ll a,b;
-    mutable ld p; // the position where this line intersects with the next line in the container
-    Line():a(0),b(0),p(LLONG_MIN){}
-    Line(ll A,ll B):a(A),b(B),p(LLONG_MIN){}
-    
+    mutable ld p; // position where this line intersects with next line in the container (aka where this line ends)
+    Line():a(0),b(0),p(LLONG_MAX){}
+    Line(ll _a,ll _b):a(_a),b(_b),p(LLONG_MAX){}
     
     ll cal(ll x) const {return a * x + b;}
     
@@ -41,18 +40,18 @@ struct Line // y = ax + b
     }
 };
 
-  // Convex Hull Trick General Container using multiset
-struct CHT_General : multiset<Line,less<void>>
+  // Line Container using multiset
+struct CHT_General : multiset<Line, less<void>>
 {
       // Calculate intersection (cut) point between two lines
-    ld xcut(iterator a,iterator b) const { return (ld)(b->b - a->b) / abs(a->a - b->a);}
+    ld xcut(iterator a,iterator b) const { return (ld)(b->b - a->b) / (a->a - b->a);}
     
-      // Checks if line a should remain before line b
-    bool check(iterator a,iterator b)
+      // Checks if line 'b' is valid
+    bool check(iterator a,iterator b) // O( 1 )
     {
         if(b == end())
         {
-            a->p = LLONG_MAX; // Set p as infinity for the last line
+            a->p = LLONG_MAX; // the last line ends at infinity
             return 1;
         }
         
@@ -62,35 +61,31 @@ struct CHT_General : multiset<Line,less<void>>
         return a->p < b->p;
     }
     
-      // Add a new line to the container, maintaining optimal structure
-    void add(Line A)
+      // Add a new line to the container & maintain structure
+    void add(Line A) // O( log(n) ) amortized
     {
         iterator a = insert(A);
         iterator b = next(a);
-        while(!check(a,b)) b = erase(b); // Remove useless lines after a
+          // Remove invalid lines behind the new line
+        while(!check(a,b)) b = erase(b); 
         
-        if(a != begin())
+          // ___ infront of the new line
+        bool forced = 1; // we must check the new line's validity AND one of the lines infront
+        while(a != begin())
         {
-            b = prev(a);
-            if(!check(b,a)) check(b,erase(a));
-        }
-        else return;
-        
-          // Check previous lines to maintain container integrity
-        while(b != begin())
-        {
-            a = prev(b);
-            if(a->p >= b->p)
-            {
-                check(a,erase(b)); // erase b and recalculate a->p
-                b = a;
-            }
-            else break;
+            b = a;
+            a = prev(a);
+            if(!check(a,b)) check(a,erase(b)); // erase 'b' and recalculate a->p
+            else if(!forced) break;
+            forced = 0;
         }
     }
     
       // Query the minimum value at x
-    ll cal(ll x) { return lower_bound(x)->cal(x); }
+    ll cal(ll x)  // O( log(n) )
+    {
+        return lower_bound(x)->cal(x); 
+    }
 };
 // End Code Convex Hull Trick - General Container
 
@@ -116,16 +111,10 @@ int main()
     
     for(int i = 1;i<=n;i++)
     {
-        if(i == 1)
-        {
-            dp[i] = 0; 
-        }
-        else
-        {
-            dp[i] = container.cal(a[i]) + a[i] * a[i] + pre[i-1];
-        }
+        if(i == 1)  dp[i] = 0; 
+        else  dp[i] = container.cal(a[i]) + a[i] * a[i] + pre[i-1];
           // Add new line for future DP queries
-        container.add(Line( a[i] * -2, a[i] * a[i] +dp[i] - pre[i] ));
+        container.add(Line( a[i] * -2, a[i] * a[i] - pre[i] + dp[i] ));
     }
     
     cout<<dp[n]; 
